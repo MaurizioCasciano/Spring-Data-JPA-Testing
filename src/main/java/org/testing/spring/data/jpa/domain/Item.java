79f67d9ca361b41b26dab2f6279e4ae013320842
Maurizio Casciano <mauriziocasciano7@gmail.com>
2022-06-10 09:22:57 +0200

    Item implements Persistable(UUID)

--- src/main/java/org/testing/spring/data/jpa/domain/Item.java
+++ src/main/java/org/testing/spring/data/jpa/domain/Item.java
@@ -3,6 +3,7 @@ package org.testing.spring.data.jpa.domain;
 import lombok.Data;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
+import org.springframework.data.domain.Persistable;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -11,10 +12,15 @@ import java.util.UUID;
 
 @Data
 @Entity
-public class Item {
+public class Item implements Persistable<UUID> {
     @Id
     private UUID id;
 
     @ManyToOne(optional = false)
     private Container container;
+
+    @Override
+    public boolean isNew() {
+        return false;
+    }
 }
